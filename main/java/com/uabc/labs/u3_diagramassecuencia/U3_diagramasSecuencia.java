414173efdcca5eb2ceb5cc06c9c3cd27270089f1
XDurango2 <130610928+XDurango2@users.noreply.github.com>
2024-03-05 12:19:39 -0800

    Initial commit

---
+++ main/java/com/uabc/labs/u3_diagramassecuencia/U3_diagramasSecuencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uabc.labs.u3_diagramassecuencia;
+
+/**
+ *
+ * @author us
+ */
+public class U3_diagramasSecuencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
